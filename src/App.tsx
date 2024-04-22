38d3821466de0c92a4ad757c856af8a57bc02553
Woojin-Choi <panda1996@snu.ac.kr>
2024-04-22 14:26:54 +0900

    Feat: first fetch using react query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CatViewer from "./CatViewer";
 import WorkingHours from "./WorkingHours";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -24,7 +28,15 @@ function App() {
           </ul>
         </nav>
         <Routes>
-          <Route path="/cat-viewer" element={<CatViewer />} />
+          <Route
+            path="/cat-viewer"
+            element={
+              <QueryClientProvider client={queryClient}>
+                <CatViewer />
+                <ReactQueryDevtools initialIsOpen={false} />
+              </QueryClientProvider>
+            }
+          />
           <Route path="/working-hour" element={<WorkingHours />} />
         </Routes>
       </div>
--- src/CatViewer.tsx
+++ src/CatViewer.tsx
@@ -1,5 +1,55 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface CatsInterface {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+  breeds: object;
+  favourite: object;
+}
+
 function CatViewer() {
-  return <div>1번 과제 - CatViewer</div>;
+  async function fetchCats() {
+    return await axios
+      .get("https://api.thecatapi.com/v1/images/search", {
+        params: { limit: 30 },
+        headers: {
+          "x-api-key":
+            "live_k77YJ1Sa3RsUfqEwbuKzrsevPSBW7iCoeeTZKSuj0ahl51TyYwbMXoLhVwwyIIvF",
+        },
+      })
+      .then((res) => res.data);
+  }
+
+  const { data, isLoading, isError } = useQuery<CatsInterface[]>({
+    queryKey: ["get-coin"],
+    queryFn: fetchCats,
+  });
+
+  if (isLoading) return <div>loading...</div>;
+
+  if (isError) return <div>Error...</div>;
+
+  return (
+    <div style={{ maxWidth: "1200px", border: "1px solid black" }}>
+      {data?.map((d) => {
+        return (
+          // <img
+          //   src={d.url}
+          //   alt="this is cat"
+          //   style={{
+          //     width: "389px",
+          //     border: "5px solid black",
+          //     height: "100%",
+          //   }}
+          // />
+          <div>{d.url}</div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default CatViewer;
